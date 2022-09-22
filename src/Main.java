bf40997b8d6453d325510cfab178258963b3d7d4
ZionBlade <wernerct@mail.uc.edu>
2022-09-22 13:14:28 -0400

    finished commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        double itemCost = 90;
+        double shipCost;
+        double total;
+
+        shipCost = itemCost * 0.02;
+        total = shipCost + itemCost;
+
+        System.out.println("what is the item cost?");
+
+        if (itemCost >= 100)
+            System.out.println("Your shipping is free!");
+        else {
+            System.out.println("Your shipping cost is " + shipCost);
+            System.out.println("Your total cost is " + total);
+        }
+
     }
 }
